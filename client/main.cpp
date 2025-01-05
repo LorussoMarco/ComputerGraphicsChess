500af0583276d4faeab6f404f35c38ce4c9beb8e
adriano.chiriaco <adriano.chiriaco@student.supsi.ch>
2025-01-05 15:38:04 +0100

    partial commit

--- client/main.cpp
+++ client/main.cpp
@@ -17,6 +17,7 @@ namespace Constants {
     constexpr inline int KEYBOARD_KEY_DOWN = 103;
     constexpr inline int KEYBOARD_KEY_LEFT = 100;
     constexpr inline int KEYBOARD_KEY_RIGHT = 102;
+    constexpr inline int KEYBOARD_KEY_ENTER = 13;
 }
 
 // Camera reference
@@ -53,7 +54,7 @@ void addLightAboveScene() {
     Engine::getScene()->addChild(lightAbove);
 }
 
-void setupCameraAndLightMovement() {
+/*void setupCameraAndLightMovement() {
     Engine::setKeyboardCallback([](const unsigned char key, const int mouseX, const int mouseY) {
         // Ottieni la posizione corrente della camera e della luce
         glm::vec3 cameraPosition = camera->getPosition();
@@ -150,12 +151,13 @@ void setupCameraAndLightMovement() {
             << rotation.y << ", "
             << rotation.z << ")" << std::endl;
         });
-}
+}*/
 
 int main() {
     // Inizializza il motore con titolo finestra, larghezza e altezza
     Engine::init("Test Scene", 800, 600);
 
+    ChessLogic::initialPopulate();
     ChessLogic::init();
 
     static std::list<std::string> stringList = {"Rooftop", "Floor", "Wall001", "Wall002", "Wall003", "Table", "Tableleg001","Tableleg002" ,"Tableleg003" ,"Tableleg004", "Omni001","ChessBoard.001"};
@@ -188,6 +190,38 @@ int main() {
             }
         });
 
+    Engine::setMethodSpecialCallback([](int key, int mouseX, int mouseY)
+        {
+            
+            switch (key) {
+            case Constants::KEYBOARD_KEY_UP:
+                ChessLogic::move(Direction::UP);
+                break;
+
+            case Constants::KEYBOARD_KEY_DOWN:
+                ChessLogic::move(Direction::DOWN);
+                break;
+
+            case Constants::KEYBOARD_KEY_LEFT:
+                ChessLogic::move(Direction::LEFT);
+                break;
+
+            case Constants::KEYBOARD_KEY_RIGHT:
+                ChessLogic::move(Direction::RIGHT);
+                break;
+            }
+
+        });
+
+    Engine::setKeyboardCallback([](const unsigned char key, const int mouseX, const int mouseY) {
+        
+        switch (key) {
+        case 13: 
+            ChessLogic::selectPiece("none");
+            break;
+        }
+    });
+
     // Crea un nodo di scena e lo imposta
     std::shared_ptr<Node> scene = std::make_shared<Node>();
     scene->setName("RootNode");
@@ -205,7 +239,7 @@ int main() {
     Engine::setActiveCamera(camera);
 
     // Imposta il movimento della camera e della luce
-    setupCameraAndLightMovement();
+    //setupCameraAndLightMovement();
 
     // Carica una scena da file OVO e la imposta
     std::shared_ptr<Node> ovoScene = OVOParser::fromFile("./scena1.ovo");
